1fd7054a8fa6a61bae50816e2f0631d16ad27a4e
emma <eleyva2@usf.edu>
2025-10-03 10:56:44 -0700

    created index

---
+++ cc_6.js
@@ -0,0 +1,12 @@
+// step 2 Creating a Base 
+class Employee {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+describe() 
+    return `${this.name} works in the ${this.department} department.`;
+    const employee1= new Employee('David', 'Fire Fighting');
+    console.log(employee1.describe()); 
+  
