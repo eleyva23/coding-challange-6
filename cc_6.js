76b18a1ae9bc44ba937b168002a59d126ce805f9
emma <eleyva2@usf.edu>
2025-10-03 11:16:12 -0700

    completed step three

--- cc_6.js
+++ cc_6.js
@@ -15,4 +15,20 @@ class Employee {
 const employee1 = new Employee('david', 'fireFighting');
 
 
-console.log(employee1.describe()); // Prints "Alice works in the Engineering department."
+console.log(employee1.describe()); //"david works in the fireFighting department."
+//Step 3 Create a SubClass Manger 
+class Manager extends Employee{
+  constructor(name, department, teamSize){
+    super(name, department);
+    this.teamSize= teamSize;
+  }
+  describe() {
+    // Override the parent's describe method
+    return `${super.describe()} They manage a team of ${this.teamSize} people.`;
+  }
+}
+
+// Example usage:
+const manager1 = new Manager("Emma Leyva", "Sales", 10);
+console.log(manager1.describe()); // 
+// Step 4 Adding new employees 
